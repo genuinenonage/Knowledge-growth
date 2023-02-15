b2a62b26b62673bf8b2950c1fb6dd3eb4ab40dae
180171 <qibo@ejianlong.com>
2023-02-15 17:16:47 +0800

    fix：处理文件路径访问异常问题并创建rocket服务

---
+++ growth-rocketmq/src/main/java/com/genuinenoange/GrowthRocketMqApplication.java
@@ -0,0 +1,11 @@
+package com.genuinenoange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrowthRocketMqApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GrowthRocketMqApplication.class, args);
+    }
+}
